0c8252617884eb8f274aa7667c261f8379397942
Emin <emin_musaev_98@mail.ru>
2026-03-04 13:22:31 +0300

    add header nav layout, and ts tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import './training.ts';
+import { Color } from '../enums/Color';
+import { Collection, nameCollection, numberCollection } from './collection';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +10,28 @@ import './training.ts';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  companyName: string = 'Румтибет'
+  
+  constructor() {
+    this.saveLastVisit();
+    this.updateVisitsCount();
+    console.log(nameCollection.getAll());
+    numberCollection.replace(1, 77);
+  }
+  
+  getColor(color: Color): boolean {
+  const primaryColors: Color[] = [Color.RED, Color.GREEN, Color.BLUE];
+  return primaryColors.includes(color);
+}
+
+saveLastVisit(): void {
+    const now: string = new Date().toString();
+    localStorage.setItem('lastVisitDate', now);
+  }
+  
+  updateVisitsCount(): void {
+    const storedValue: number = Number(localStorage.getItem('visitsCount') || 0);
+    localStorage.setItem('visitsCount', String(storedValue + 1));
+  }
 
 }
