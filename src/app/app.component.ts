190199b2fe53b69cff31c8f5c84c8c2a438deba2
Emin <emin_musaev_98@mail.ru>
2026-03-10 01:36:29 +0300

    fix code formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,28 +10,27 @@ import { Collection, nameCollection, numberCollection } from './collection';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  companyName: string = 'Румтибет'
-  
-  constructor() {
-    this.saveLastVisit();
-    this.updateVisitsCount();
-    console.log(nameCollection.getAll());
-    numberCollection.replace(1, 77);
-  }
-  
-  getColor(color: Color): boolean {
-  const primaryColors: Color[] = [Color.RED, Color.GREEN, Color.BLUE];
-  return primaryColors.includes(color);
-}
+    companyName: string = 'Румтибет';
 
-saveLastVisit(): void {
-    const now: string = new Date().toString();
-    localStorage.setItem('lastVisitDate', now);
-  }
-  
-  updateVisitsCount(): void {
-    const storedValue: number = Number(localStorage.getItem('visitsCount') || 0);
-    localStorage.setItem('visitsCount', String(storedValue + 1));
-  }
+    constructor() {
+        this.saveLastVisit();
+        this.updateVisitsCount();
+        console.log(nameCollection.getAll());
+        numberCollection.replace(1, 77);
+    }
 
+    isPrimaryColor(color: Color): boolean {
+        const primaryColors: Color[] = [Color.RED, Color.GREEN, Color.BLUE];
+        return primaryColors.includes(color);
+    }
+
+    saveLastVisit(): void {
+        const now: string = new Date().toString();
+        localStorage.setItem('lastVisitDate', now);
+    }
+
+    updateVisitsCount(): void {
+        const storedValue: number = Number(localStorage.getItem('visitsCount') || 0);
+        localStorage.setItem('visitsCount', String(storedValue + 1));
+    }
 }
